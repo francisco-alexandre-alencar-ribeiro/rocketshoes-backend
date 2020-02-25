2cd1ff82b2d92b1e8edd2e297187db189ef7c381
alexandre alencar <alexandreribeiro685@gmail.com>
2020-02-25 20:06:45 -0300

    Rename files

--- src/main/java/com/spring/rest/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.rest.repository;
-
-import com.spring.rest.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Created by alexandre on 23/02/2020.
- */
-public interface PostRepository extends JpaRepository<Post, Long> {}
